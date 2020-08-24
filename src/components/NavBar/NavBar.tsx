b648b434704a9e0c4378db7b3d9a856c037a393b
0kMike <m.naumann@windot.de>
2020-08-24 21:59:44 +0200

    Create NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./NavBar.module.css"
+
+interface INavBar {
+
+}
+
+const NavBar: React.FC = (props: INavBar) => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default NavBar;
